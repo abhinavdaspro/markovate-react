86f96bd991c80d04b901c04a30000284ab50e709
Abhinav Das <abhinavdaspro@gmail.com>
2023-05-26 18:19:56 +0530

    layout

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,20 +2,17 @@ import React from 'react'
 
 export const SignUp = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p className='text-red-400 font-semibold'>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className='flex h-screen w-full flex-col bg-white'>
+            <div className='h-16 w-full bg-red-200 flex items-center px-16'>
+                <p>BrandName</p>
+            </div>
+            <div className=" w-full bg-white py-12 flex justify-center">
+
+                <div className='max-w-2xl w-full h-auto rounded shadow-lg p-20'>
+
+                </div>
+
+            </div>
         </div>
     )
 }
