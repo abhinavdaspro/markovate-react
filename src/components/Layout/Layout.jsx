3aecf9ced4aff831ef8d4aecae0467fdcd84ba8a
Abhinav Das <abhinavdaspro@gmail.com>
2023-05-27 02:14:42 +0530

    responsive layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -14,13 +14,19 @@ const Layout = () => {
                     <Outlet />
                 </div>
             </div>
-            <div className='flex flex-col h-auto lg:h-screen  w-full lg:w-[30%] mt-auto lg:ml-auto bg-dark-2 items-center'>
-                <div className='w-36 h-36 mt-20 border-[2px] rounded-full border-white shadow-lg shadow-gray-950'>
-                    <img src={Profile} alt="profile" />
+            <div className='flex flex-row lg:flex-col h-auto lg:h-screen  w-full lg:w-[30%] mt-auto lg:ml-auto bg-dark-2 items-center px-5 sm:px-[5%] lg:px-0'>
+                <div className='lg:mt-10 flex flex-col items-center justify-center w-[50%] lg:w-full'>
+                    <div className='w-20 lg:w-36 h-20 lg:h-36 mt-10 lg:mt-20 border-[2px] rounded-full border-white shadow-lg shadow-gray-950'>
+                        <img src={Profile} alt="profile" />
+                    </div>
+                    <p className='text-white font-medium text-xs lg:text-base mt-5 lg:mt-10'>Edward Pascual, PSM</p>
+                    <p className='text-gray-500 text-xs lg:text-sm mt-2 lg:mt-5 text-center mb-10 lg:mb-0'>Project Manager at Trapeze Group</p>
                 </div>
-                <p className='text-white font-medium text-base mt-10'>Edward Pascual, PSM</p>
-                <p className='text-gray-500 text-sm mt-5'>Project Manager at Trapeze Group</p>
-                <p className='text-white text-center mt-20 px-[20%]'>“ The excellent design and stability of the app has pleased internal stakeholders. We are very happy with their service. ”</p>
+                <div className='flex justify-center w-[50%] lg:w-full'>
+                    <p className='text-white text-center lg:mt-20  lg:px-[20%]  w-[60%] lg:w-auto text-xs sm:text-base'>“ The excellent design and stability of the app has pleased internal stakeholders. We are very happy with their service. ”</p>
+                </div>
+
+
             </div>
         </div>
     )
